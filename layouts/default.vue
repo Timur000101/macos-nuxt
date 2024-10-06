b171b3f25b02791ea021914a8b343e0f9e5f3fb4
Temur Kuanayev <tima000101@gmail.com>
2024-10-06 21:46:49 +0500

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="header">
+        Header
+    </div>
+    <div class="content">
+        <slot />
+    </div>
+    <div class="footer">
+        Footer
+    </div>
+</template>
